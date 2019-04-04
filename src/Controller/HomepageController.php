4199975ade2d28bfbb080895396464a3f3739b36
Anthony Destenay <anthony.destenay@gmail.com>
2019-04-04 10:12:58 +0200

    Added dashboard page for user

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
@@ -15,5 +16,14 @@ class HomepageController extends AbstractController
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     * @IsGranted("ROLE_USER")
+     */
+    public function dashboard()
+    {
+        return $this->render('homepage/dashboard.html.twig');
     }
 }
