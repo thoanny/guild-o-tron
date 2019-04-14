663a98f4c97b4f16267ef737552cddb772aaa0a0
Anthony Destenay <anthony.destenay@gmail.com>
2019-04-14 19:21:21 +0200

    Fixed guild logs (order by lid)

--- src/Controller/GuildController.php
+++ src/Controller/GuildController.php
@@ -278,15 +278,13 @@ class GuildController extends AbstractController
       $api = new Gw2Api();
       $entityManager = $this->getDoctrine()->getManager();
 
-      $guildLogs = $entityManager->getRepository(GuildLog::class)->findByGuild($guild);
-
+      $guildLogs = $entityManager->getRepository(GuildLog::class)->findByGuild($guild, ['lid' => 'DESC']);
 
       if($guildLogs) {
         $newLogs = $api->get('/guild/:id/log', $guild->getToken(), ['id' => $guild->getGid()], ['since' => $guildLogs[0]->getLid()]);
       } else {
         $newLogs = $api->get('/guild/:id/log', $guild->getToken(), ['id' => $guild->getGid()]);
       }
-
 
       if($newLogs) {
         foreach($newLogs as $log) {
@@ -307,7 +305,7 @@ class GuildController extends AbstractController
         $entityManager->flush();
       }
 
-      $guildLogs = $entityManager->getRepository(GuildLog::class)->findByGuild($guild);
+      $guildLogs = $entityManager->getRepository(GuildLog::class)->findByGuild($guild, ['lid' => 'DESC']);
 
       return $guildLogs;
 
